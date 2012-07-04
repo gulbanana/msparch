6c802d236ed6446c7305602153f712101083f695
Thomas Castiglione <castiglione@gmail.com>
2012-07-04 15:24:34 +0800

    story default fix

--- archive.py
+++ archive.py
@@ -7,31 +7,46 @@ from urllib.parse import urlparse
 
 #constants
 site_prefix = 'http://www.mspaintadventures.com/'
+appdir = os.path.dirname(sys.argv[0])
 
+#filesystem helpers
+def _load_template(filename):
+    with open(os.path.join(appdir, 'templates', filename), 'r') as template_file:
+        return template_file.read()
 
+def _load_binary(directory, filename):
+    with open(os.path.join(appdir, directory, filename), 'rb') as f:
+        return f.read()
+
+def _save_binary(directory, filename, data):
+    with open(os.path.join(appdir, directory, filename), 'wb') as f:
+        f.write(data)
+
+def _exists(directory, filename):
+    return os.path.exists(os.path.join(appdir, directory, filename))
+
+def _get_global(filename):
+    path = os.path.join(appdir, filename)
+    if not os.path.exists(path):
+        urlretrieve(site_prefix+filename, path)
+
+# archiver which maintains a layout like that of mspa.com
 class MirroringArchiver:
     def __init__(self, storyid):
         self.story = storyid
         self.root = stories.dirs(self.story)[0]
 
-        with open('templates/page.txt', 'r') as template_file:
-            self._html_template = template_file.read()
-        with open('templates/flash.txt', 'r') as template_file:
-            self._object_template = template_file.read()
-        with open('templates/pesterlog.txt', 'r') as template_file:
-            self._log_template = template_file.read()
+        self._html_template = _load_template('page.txt')
+        self._object_template = _load_template('flash.txt')
+        self._log_template = _load_template('pesterlog.txt')
 
         os.makedirs('images', exist_ok=True)
-        self.__get_global('images/logo.gif')
-        self.__get_global('images/title.png')
+        _get_global('images/logo.gif')
+        _get_global('images/title.png')
 
         os.makedirs('{0}'.format(self.story), exist_ok=True)
         for directory in stories.dirs(self.story):
             os.makedirs(directory, exist_ok=True)
-
-    def __get_global(self, filename):
-        if not os.path.exists(filename):
-            urlretrieve(site_prefix+filename, filename)
 
     def finalise(self):
         pages = filter(lambda path: path.endswith('.html'), os.listdir(str(self.story)))
@@ -39,34 +54,34 @@ class MirroringArchiver:
         for page in pages:
             with open('{0}/{1}'.format(self.story, page), 'r') as pagefile:
                 text = pagefile.read()
-                text = re.sub(r'>((jb2_)?\d*)</a>', lambda match: '>{0}</a>'.format(self.__page_command(match.group(1))), text)
+                text = re.sub(r'>((jb2_)?\d*)</a>', lambda match: '>{0}</a>'.format(self._page_command(match.group(1))), text)
 
             with open('{0}/{1}'.format(self.story, page), 'w') as pagefile:
                 pagefile.write(text)
 
     ### pages ###
     def save_page(self, page, data):
-        with open('{0}/{1}.txt'.format(self.story, page), 'wb') as f:
-            f.write(data)
+        _save_binary(self.story, self._page_filename(page), data)
 
     def page_exists(self, page):
-        return os.path.exists('{0}/{1}.txt'.format(self.story, page))
+        return _exists(self.story, self._page_filename(page))
 
     def load_page(self, page):
-        with open('{0}/{1}.txt'.format(self.story, page), 'rb') as f:
-            return f.read()
+        return _load_binary(self.story, self._page_filename(page))
 
-    def __page_command(self, page):
-        with open('{0}/{1}.txt'.format(self.story, page), 'r') as f:
+    def _page_command(self, page):
+        with open(os.path.join(appdir, self.story, self._page_filename(page)), 'r') as f:
             return f.readline().strip()
+
+    def _page_filename(self, page):
+        return '{0}.txt'.format(page)
 
     ### images ###
     def save_image(self, image, data):
-        with open('{0}/{1}'.format(self.root, image), 'wb') as f:
-            f.write(data)
+        _save_binary(self.root, image, data)
 
     def image_exists(self, image):
-        return os.path.exists('{0}/{1}'.format(self.root, image))
+        return _exists(self.root, image)
 
     ### flash animations ###
     def save_flash(self, flashid, script, flash):
@@ -98,54 +113,54 @@ class MirroringArchiver:
         print('>',command)
         sys.stdout.flush()
 
-        images = map(self.__format_asset, assets)
-        anchors = map(self.__format_anchor, links)
-        content = map(self.__rewrite_links, content)
-        content = self.__rewrite_dialogue(list(content))
+        images = map(self._format_asset, assets)
+        anchors = map(self._format_anchor, links)
+        content = map(self._rewrite_links, content)
+        content = self._rewrite_dialogue(list(content))
 
         html = self._html_template.format(command=command, assets='<br/>\n<br/>\n'.join(images), narration='<br/>\n'.join(content), navigation=''.join(anchors))
     
         with open('{0}/{1}.html'.format(self.story, page), 'w') as f:
             f.write(html)
 
-    def __format_asset(self, url):
+    def _format_asset(self, url):
         if url.endswith('.gif') or url.endswith('.GIF'):
-            return self.__format_image(url)
+            return self._format_image(url)
         elif url.startswith('F|'):
-            return self.__format_flash(url[2:])
+            return self._format_flash(url[2:])
         else:
             raise Exception('unrecognised asset type '+url)
 
-    def __format_image(self, asset_uri):
+    def _format_image(self, asset_uri):
         return '<img src="../{0}"/>'.format(asset_uri[len(site_prefix):])
 
-    def __format_flash(self, flash_uri):
+    def _format_flash(self, flash_uri):
         flashid = urlparse(flash_uri).path.split('/')[-1]
         return self._object_template.format(
             id='../{0}/{1}/{1}'.format(self.root,flashid), 
             swf='../{0}/{1}/{1}.swf'.format(self.root,flashid), 
             js='../{0}/{1}/AC_RunActiveContent.js'.format(self.root,flashid))
 
-    def __format_anchor(self, page):
+    def _format_anchor(self, page):
         return '<font size="5">&gt; <a href="{0}.html">{0}</a></font><br>'.format(page)
 
-    def __format_internal_page(self, match):
+    def _format_internal_page(self, match):
         story = match.group(1)
         page = match.group(3)
         if page == None:
             page = '{0:06}'.format(stories.first_page(story))
         return '../{0}/{1}.html'.format(story, page)
 
-    def __format_internal_image(self, match):
+    def _format_internal_image(self, match):
         filename = match.group(1)
         return '../{0}"'.format(filename)
 
-    def __rewrite_links(self, text):
-        text = re.sub(site_prefix+r'\?s=(\d*)(&amp;p=(\d*))?', self.__format_internal_page, text)
-        text = re.sub(site_prefix+r'(.*)"', self.__format_internal_image, text)
+    def _rewrite_links(self, text):
+        text = re.sub(site_prefix+r'\?s=(\d*)(&amp;p=(\d*))?', self._format_internal_page, text)
+        text = re.sub(site_prefix+r'(.*)"', self._format_internal_image, text)
         return text
 
-    def __rewrite_dialogue(self, text):
+    def _rewrite_dialogue(self, text):
         logtype = re.match(r'\|(.*)\|', text[0])
 
         if logtype != None:
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import stories
 #parse args
 cmdline = argparse.ArgumentParser(description='Download MS Paint Adventures stories in an archival format.', add_help=True)
 arguments = cmdline.add_argument_group('arguments')
-arguments.add_argument('story', default=6, nargs='?', type=str, help='Story to download - defaults to 6 for Homestuck')
+arguments.add_argument('story', default='6', nargs='?', type=str, help='Story to download - defaults to 6 for Homestuck')
 arguments.add_argument('page', type=int, default=0, nargs='?', help='Page on which to start download - defaults to start of story')
 
 args = cmdline.parse_args()
