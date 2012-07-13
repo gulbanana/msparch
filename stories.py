44897f6114a571eb49c882d9843a7cc24c7dd7a2
Thomas Castiglione <castiglione@gmail.com>
2012-07-13 21:05:52 +0800

    bugfix: extras/ linked from HS

--- stories.py
+++ stories.py
@@ -26,7 +26,7 @@ def dirs(story):
     elif story == '5':
         return ['storyfiles/hs']
     elif story == '6':
-        return ['storyfiles/hs2', 'storyfiles/hs2/scraps', 'storyfiles/hs2/scratch', 'scraps', 'scraps2']
+        return ['storyfiles/hs2', 'storyfiles/hs2/scraps', 'storyfiles/hs2/scratch', 'scraps', 'scraps2', 'extras']
     elif story == 'ryanquest':
         return ['ryanquest']
     else:
